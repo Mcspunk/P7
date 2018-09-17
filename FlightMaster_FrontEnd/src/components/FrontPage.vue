9889a900ca6b353166b3cf1a467878fdbc2e806b
Patrick Alminde <palmin15@student.aau.dk>
2018-09-17 09:13:16 +0200

    FrontEnd and API setup

---
+++ FlightMaster_FrontEnd/src/components/FrontPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{welcomeText}}</h1>
+    <el-switch v-model="switchValue" v-on:change="welcomeTextPicker()"> </el-switch>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "FrontPage",
+  data() {
+    return {
+      switchValue: true,
+      welcomeText: "Så kører vi"
+    };
+  },
+  methods:{
+    welcomeTextPicker:function(){
+      if(this.switchValue === true){
+        this.welcomeText = "Så kører vi";
+      }
+      else{
+        this.welcomeText = "Vi kører ikke mere";
+      }
+    }
+  }
+};
+</script>
+
