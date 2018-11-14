61e5328f7c400f84594921cd6d4a98df000e3cc4
Lundhede <m010795@hotmail.com>
2018-11-14 11:08:07 +0100

    Nu med gennemsnit

--- LeagueDrafter_RESTAPI/initial_win_pred.py
+++ LeagueDrafter_RESTAPI/initial_win_pred.py
@@ -26,29 +26,38 @@ test_labels = np.array(wins[ta:te])
 validate_labels = np.array(wins[te:])
 
 
-batch_list = [16, 32]
-epochs_list= [1, 2]
+batch_list = [16, 32, 64, 128, 256]
+epochs_list= [5, 10, 20]
 results = []
-
+temp_test_acc=0
 # --- NN
 for i in batch_list:
     for k in epochs_list:
-        model = keras.Sequential([
-            keras.layers.Dense(150, input_shape=(10, 8)),
-            keras.layers.Dense(300, activation=tf.nn.relu, kernel_regularizer=keras.regularizers.l1(0.1)),
-            keras.layers.Dense(2, activation=tf.nn.softmax)
-            ])
+        for z in range(0, 5):
+            model = keras.Sequential([
+                keras.layers.Dense(150, input_shape=(10, 8)),
+                keras.layers.Dense(300, activation=tf.nn.relu, kernel_regularizer=keras.regularizers.l1(0.1)),
+                keras.layers.Dense(2, activation=tf.nn.softmax)
+                ])
 
-        model.compile(optimizer=keras.optimizers.Adam(lr=0.001, decay=0.1),
-                      loss='mean_squared_error',
-                      metrics=['accuracy'])
+            model.compile(optimizer=keras.optimizers.Adam(lr=0.001, decay=0.1),
+                          loss='mean_squared_error',
+                          metrics=['accuracy'])
 
-        model.fit(train_data, train_labels, epochs=k, batch_size=i)
+            model.fit(train_data, train_labels, epochs=k, batch_size=i)
+            test_loss, test_acc = model.evaluate(test_data, test_labels)
+            temp_test_acc += test_acc
 
-        test_loss, test_acc = model.evaluate(test_data, test_labels)
-
-        results.append([i, k, test_acc])
+        results.append([i, k, temp_test_acc/5])
+        temp_test_acc = 0
 
         #print('Test acc: ', test_acc)
 
 print(results)
+ddd = 0
+res = results[0]
+for i in results:
+    if i[2] > ddd:
+        ddd = i[2]
+        res = i
+print(res)
